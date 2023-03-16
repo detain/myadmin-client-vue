faecfae5d3161fc27d9aef5dd8f26c301a986661
Joe Huss <detain@interserver.net>
2023-03-16 04:18:35 -0400

    initial import

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/dist/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/dist/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/dist/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,60 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      login: '',
+      passwd: '',
+      users: []
+    };
+  },
+  methods: {
+    async LoginUser() {
+      try {
+        const user = await axios.post(
+          "http://mystage.interserver.net/apiv2/login",
+          {
+            login: this.login,
+            passwd: this.passwd
+          }
+        );
+
+        console.log(user)
+      } catch(e) {
+        console.log(e);
+      }
+    },
+    async getUsers() {
+      try {
+        const users = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        this.users = users.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+<div class="user-add">
+  <h2>User Login</h2>
+  <input type="text" v-model="login" name="login" id="login"/> <br />
+  <input type="password" v-model="passwd" name="passwd" id="passwd"/> <br />
+
+  <button @click="LoginUser">Login</button>
+</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
