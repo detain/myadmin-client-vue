8c94edce4307530b0f25d6b1a8d2808509439679
Joe Huss <detain@interserver.net>
2023-04-17 19:51:41 -0400

    added dns routes

---
+++ src/router/dns.routes.js
@@ -0,0 +1,10 @@
+import { Layout, DnsManager, DnsEditor } from '@/views/dns'
+
+export default {
+    path: '/dns',
+    component: Layout,
+    children: [
+        { path: '', component: DnsManager },
+        { path: ':id(\\d+)', component: DnsEditor }
+    ]
+};
