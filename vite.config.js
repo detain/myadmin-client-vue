f48f4661c08adbd05e07a76bdb586ac16206be9f
Joe Huss <detain@interserver.net>
2023-04-18 06:33:46 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import { splitVendorChunkPlugin } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import vue from '@vitejs/plugin-vue'
 //import AutoImport from 'unplugin-auto-import'
@@ -8,9 +9,10 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.ELECTRON=="true" ? './' : ".",
+    base: process.env.ELECTRON=="true" ? './' : "/",
   plugins: [
     vue(),
+    splitVendorChunkPlugin(),
     i18nResources({
       path: resolve(__dirname, "src/locales"),
     }),
