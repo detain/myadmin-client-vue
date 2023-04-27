a0f9774e8c66d0c8c82263f48adc0729e593ca61
Joe Huss <detain@interserver.net>
2023-04-27 14:56:48 -0400

    work on the order forms

--- src/views/backups/OrderBackup.vue
+++ src/views/backups/OrderBackup.vue
@@ -1,26 +1,207 @@
 <script setup>
+
+const periods = [
+  { label: 'Monthly', value: 1 },
+  { label: '3 Months', value: 3 },
+  { label: '6 Months (5% off)', value: 6 },
+  { label: 'Yearly (10% off)', value: 12 },
+  { label: '24 Months (15% off)', value: 24 },
+  { label: '36 Months (20% off)', value: 36 },
+];
 </script>
 
 <template>
-<div class="row justify-content-center mt-5">
-  <div class="col-md-10 text-center">
-    <h3 class="pb-2 text-capitalize">Find your backup and check availability.</h3>
-    <form method="post" action="backup_order" class="search-backup">
-      <input type="hidden" name="csrf_token" value="2f0b23ce49b8c42f3559107eb965b346987e5a1495492dd9cb9ae45a03b0c51e26eff52bf9ff9c38d02efb88150e61051fd123a93540674e36c46c92ae4a229d">
-      <div class="form-group row justify-content-center">
-        <div class="col-md-5 input-group pb-2">
-          <input id="backup_search" type="text" class="form-control" name="hostname" value="" autofocus="autofocus" onfocus=" let value = this.value; this.value = null; this.value=value" autocomplete="off" style="border-radius: 5px;">
+    <template v-if="step == 'orderform'">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="p-1">
+                            <h3 class="card-title py-2"><i class="fas fa-database" aria-hidden="true">&nbsp;</i>Order Storage</h3>
+                            <div class="card-tools float-right">
+                                <button type="button" class="btn btn-tool mt-0" data-card-widget="collapse"><i class="fas fa-minus" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form id="storage_form" method="post" class="storage_form_init" @submit.prevent="handleSubmit">
+                            <input type="hidden" name="csrf_token" :value="csrfToken" />
+                            <input type="hidden" name="rootpass" :value="rootpass" />
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Package&nbsp;<span class="text-danger">*</span></label>
+                                <div class="col-sm-10 input-group">
+                                    <select v-model="selectedPackage" class="form-control form-control-sm select2">
+                                        <option v-for="package in packages" :key="package.value" :value="package.value">{{ package.label }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Billing Cycle&nbsp;<span class="text-danger">*</span></label>
+                                <div class="col-sm-10">
+                                    <select v-model="selectedPeriod" class="form-control form-control-sm select2" @change="updatePrice">
+                                        <option v-for="period in periods" :key="period.value" :value="period.value" :selected="selectedPeriod === period.value">{{ period.label }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div id="coupon_row" class="row">
+                                <label class="col-md-2 col-form-label">Coupon Code</label>
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control form-control-sm" v-model="couponCode" @change="updateCoupon" placeholder="Coupon Code" />
+                                </div>
+                                <div class="offset-md-2 col-md-10">
+                                    <img :src="`https://my.interserver.net/validate_coupon.php?module=vps'`" id="couponimg" height="20" width="20" />
+                                </div>
+                            </div>
+                            <hr class="mt-0" />
+                            <div class="row">
+                                <div class="text-center col-md-12">
+                                    <input type="submit" name="Submit" value="Continue" class="btn btn-order btn-sm py-2 px-3" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="p-1">
+                            <h4 class="card-title py-2">
+                                <i class="fa fa-shopping-cart">&nbsp;</i>Order Summary
+                            </h4>
+                            <div class="card-tools float-right">
+                                <button type="button" class="btn btn-tool mt-0" data-card-widget="collapse"><i class="fas fa-minus" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-md">
+                        <div class="row mb-3">
+                            <div class="col-md-6 package_name"></div>
+                            <div class="col text-right">
+                                <span class="period">1</span> Month(s)
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div id="hostname_display" class="col-md-6">Package Cost</div>
+                            <div class="col text-right package_cost"></div>
+                        </div>
+                        <div class="row mb-3 cyclediscountrow">
+                            <div class="col-md-6">Billing cycle Discount</div>
+                            <div id="cyclediscount" class="col text-right"></div>
+                        </div>
+                        <div id="couponpricerownew" class="row mb-3 coupon-display">
+                            <div id="couponpricetext" class="col-md-6"></div>
+                            <div id="couponprice" class="col text-right"></div>
+                        </div>
+                        <hr>
+                        <div class="row mb-3">
+                            <div class="col-md-8 text-lg">Total</div>
+                            <div id="totalprice" class="col text-lg text-right total_cost"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="form-group row">
-        <div class="controls col-md-12" style="text-align: center;">
-          <button type="submit" class="btn btn-custom mr-2 px-4 py-2 text-sm">Search</button>
-          <a target="blank" href="https://interserver.net/backups" class="btn btn-order px-3 py-2 text-sm">Check Prices</a>
+    </template>
+    <template v-else-if="step == 'order_confirm'">
+        <div class="row justify-content-center">
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="p-1">
+                            <h4 class="card-title py-2">
+                                <i class="fa fa-shopping-cart" aria-hidden="true">&nbsp;</i>Order Summary
+                            </h4>
+                            <div class="card-tools float-right">
+                                <button type="button" class="btn btn-tool mt-0" data-card-widget="collapse"><i class="fas fa-minus" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" id="edit_order_form" :action="orderStorageUrl">
+                            <input type="hidden" name="csrf_token" :value="csrfToken" />
+                            <input v-for="(value, key) in orderData" :key="key" type="hidden" :id="key" :name="key" :value="value" />
+                        </form>
+                        <form method="post" class="storage_form_confirm" :action="orderStorageUrl">
+                            <input type="hidden" name="csrf_token" :value="csrfToken" />
+                            <input v-for="(value, key) in orderData.data" :key="key" :id="(key == 'backup') ? 'backupselect' : key" type="hidden" :name="key" :value="value" />
+                            <table class="table table-sm table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <div class="text-md float-left" style="position: relative;top:5px;">
+                                                {{ serviceDetail.services_name }}
+                                            </div>
+                                            <button type="button" class="btn btn-custom btn-sm float-right" name="update_values" @click="editForm" data-toggle="tooltip" title="Edit details">
+                                                <i class="fa fa-pencil"></i>&nbsp;Edit
+                                            </button>
+                                        </th>
+                                        <th class="text-md">{{ orderData.data.period }} Month(s)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-md">Package Cost</td>
+                                        <td>
+                                            <div class="text-bold text-md package_cost"></div>
+                                        </td>
+                                    </tr>
+                                    <template v-if="orderData.data.coupon">
+                                        <tr>
+                                            <td class="text-md">Coupon Used</td>
+                                            <td class="text-bold text-md">
+                                                {{ orderData.data.coupon }}
+                                                <img src="https://my.interserver.net/validate_coupon.php?module=webhosting'" style="padding-left: 10px;" id="couponimg" height="20" width="20">
+                                            </td>
+                                        </tr>
+                                    </template>
+                                    <tr id="couponpricerownew">
+                                        <td id="couponpricetext" class="text-md">Coupon Discount</td>
+                                        <td class="text-bold text-md">
+                                            <div id="couponprice"></div>
+                                        </td>
+                                    </tr>
+                                    <tr class="cyclediscountrow">
+                                        <td id="couponpricetext" class="text-md">Billing Cycle Discount</td>
+                                        <td class="text-bold text-md">
+                                            <div id="cyclediscount"></div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>
+                                            <div class="text-lg">Total</div>
+                                        </th>
+                                        <th>
+                                            <div class="text-lg total_cost"></div>
+                                        </th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <hr>
+                            <div class="p-1">
+                                <h4 class="text-center"><u>Agree to the offer terms</u></h4>
+                                <p class="text-center text-sm">
+                                    The subscription will automatically renew after <b>every month at</b>
+                                    <span class="text-bold">{{ currencySymbol }}{{ orderData.data.renewal_cost || tosRepeatServiceCost }}</span> until canceled.
+                                </p>
+                                <p class="text-muted text-xs">
+                                    By checking this box, you acknowledge that you are purchasing a subscription product that automatically renews <b>( As Per The Terms Outlined Above )</b> and is billed to the credit card you provide today. If you wish to cancel your auto-renewal, you may access the customer portal <a href="https://my.interserver.net" target="__blank" class="link">(Here)</a> select the active service and click the <b>Cancel</b> link or email at: <a href="mailto:billing@interserver.net" class="link">billing@interserver.net</a> or use another method outlined in the <b>Terms and Conditions.</b> By checking the box and clicking Place My Order below, You also acknowledge you have read, understand, and agree to our <a class="link" href="https://www.interserver.net/terms-of-service.html" target="__blank">Terms and Conditions</a> and <a class="link" href="https://www.interserver.net/privacy-policy.html" target="__blank">Privacy Policy</a>.
+                                </p>
+                                <div class="icheck-success text-bold text-center"><input type="checkbox" name="tos" id="tos" style="margin: 0 5px; display: inline;" value="yes">
+                                    <label for="tos" class="d-inline text-center">I have read the terms above and I agree.</label>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="text-center col-md-12"><input type="submit" name="Submit" value="Place Order" class="btn btn-green btn-sm px-3 py-2"></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </form>
-  </div>
-</div>
+    </template>
 </template>
 
 <style scoped>
