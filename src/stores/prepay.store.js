bb53ce65af7c3e156ff154aeb771a0c5d33f67f9
Joe Huss <detain@interserver.net>
2023-05-09 05:45:10 -0400

    added prepay store

---
+++ src/stores/prepay.store.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia';
+import { fetchWrapper, snakeToCamel } from '@/helpers';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}/accounts`;
+
+export const usePrePayStore = defineStore({
+    id: 'prepay',
+    state: () => ({
+        loading: false,
+        error: false,
+        custid: "",
+        ima: "client",
+        csrf_token: "",
+        modules: {},
+        prepays: {},
+        total_pages: 0,
+        total_records: 0,
+        limit: 10,
+        page: 1,
+        curr_page_records: 0,
+        allInfo: {}
+    }),
+    actions: {
+        async register(user) {
+            await fetchWrapper.post(`${baseUrl}/register`, user);
+        },
+        async load() {
+            const keyMap = {
+                'package': 'pkg',
+            };
+            /*
+            this.user = { loading: true };
+            try {
+                this.user = await fetchWrapper.get(`${baseUrl}/${id}`);
+            } catch (error) {
+                this.user = { error };
+            }
+            */
+            try {
+                const response = await fetchWrapper.get('https://mystage.interserver.net/apiv2/billing/prepays');
+                this.$reset();
+                let key, value;
+                console.log('api success');
+                console.log(response);
+                for (key in response) {
+                    value = response[key];
+                    if (typeof this[key] != 'undefined') {
+                        this[key] = value;
+                    } else if (typeof this[snakeToCamel(key)] != 'undefined') {
+                        this[snakeToCamel(key)] = value;
+                    } else if (typeof keyMap[key] != 'undefined') {
+                        this[keyMap[key]] = value;
+                    } else {
+                        console.log("no key '"+key+"' with value '"+value+"'");
+                    }
+                }
+            } catch (error) {
+                console.log('api failed');
+                console.log(error);
+            }
+
+
+        },
+        async update(id, params) {
+            await fetchWrapper.put(`${baseUrl}/${id}`, params);
+
+            // update stored user if the logged in user updated their own record
+            const authStore = useAuthStore();
+            if (id === authStore.user.id) {
+                // update local storage
+                const user = { ...authStore.user, ...params };
+                localStorage.setItem('user', JSON.stringify(user));
+
+                // update auth user in pinia state
+                authStore.user = user;
+            }
+        },
+        async delete(id) {
+            // add isDeleting prop to user being deleted
+            this.accountList.find(x => x.id === id).isDeleting = true;
+
+            await fetchWrapper.delete(`${baseUrl}/${id}`);
+
+            // remove user from list after deleted
+            this.accountList = this.accountList.filter(x => x.id !== id);
+Z        }
+    }
+});
