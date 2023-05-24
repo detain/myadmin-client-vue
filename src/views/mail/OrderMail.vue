9b7d5b392173a4cb70ff4951cd65000c723d3db0
Joe Huss <detain@interserver.net>
2023-05-24 11:42:57 -0400

    fixing some img src links in order mail

--- src/views/mail/OrderMail.vue
+++ src/views/mail/OrderMail.vue
@@ -102,7 +102,7 @@ mailOrderStore.load();
                                 <label class="col-md-3 col-form-label text-right">Coupon Code</label>
                                 <div class="col-md-9">
                                     <input type="text" class="form-control form-control-sm" v-model="coupon" @input="update_coupon()" placeholder="Coupon Code">
-                                    <span class="input-group-addon" style="padding: 0"><img src="https://my.interserver.net/validate_coupon.php?module=vps'" id="couponimg" height="20" width="20" alt=""></span>
+                                    <span class="input-group-addon" style="padding: 0"><img src="https://mystage.interserver.net/validate_coupon.php?module=vps" id="couponimg" height="20" width="20" alt=""></span>
                                 </div>
                             </div>
                             <div class="row">
@@ -205,7 +205,7 @@ mailOrderStore.load();
                                             <div class="text-md">Coupon Used</div>
                                         </td>
                                         <td>
-                                            <div class="text-bold text-md">{{ coupon }} <img src="{ 'https://my.interserver.net/validate_coupon.php?module=webhosting&coupon=' + validateResponse.coupon }" style="padding-left: 10px;" id="couponimg" height="20" width="20" alt=""></div>
+                                            <div class="text-bold text-md">{{ coupon }} <img :src="'https://mystage.interserver.net/validate_coupon.php?module=webhosting&coupon=' + validateResponse.coupon" style="padding-left: 10px;" id="couponimg" height="20" width="20" alt=""></div>
                                         </td>
                                     </tr>
                                     <tr style="display: none;">
