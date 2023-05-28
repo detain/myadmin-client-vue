db72c87a55b0d06ee9205886d5af60dcd134dbb3
Joe Huss <detain@interserver.net>
2023-05-28 15:41:15 -0400

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import { VueQueryPlugin } from "vue-query"
 import { createHead } from "@unhead/vue"
 import { VueRecaptchaPlugin } from 'vue-recaptcha'
+//import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import { router } from './router'
@@ -47,7 +48,7 @@ import '@fortawesome/fontawesome-free/js/all.min.js';
 const app = createApp(App)
 const pinia = createPinia()
 const head = createHead()
-
+//const i18n = createI18n({})
 app.use(pinia)
 app.use(router)
 app.use(VueQueryPlugin)
@@ -56,4 +57,5 @@ app.use(VueRecaptchaPlugin, {
   v2SiteKey: '6LeYMVkUAAAAAOW7Nw0e9rhAxIfH5T9k-JN9pMr2',
 //  v3SiteKey: 'YOUR_V3_SITEKEY_HERE',
 })
+//app.use(i18n)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,46 @@
 import { defineConfig } from 'vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import { splitVendorChunkPlugin } from 'vite'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 import vue from '@vitejs/plugin-vue'
 import Inspect from 'vite-plugin-inspect'
 import Inspector from 'vite-plugin-vue-inspector'
 //import Inspector from 'unplugin-vue-inspector/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import i18nResources from "vite-plugin-i18n-resources"
-import checker from 'vite-plugin-checker'
+//import i18nResources from "vite-plugin-i18n-resources"
+//import checker from 'vite-plugin-checker'
 import { resolve } from "path"
 import { fileURLToPath, URL } from 'node:url'
 import inject from '@rollup/plugin-inject';
 import { VitePWA } from 'vite-plugin-pwa'
 //import legacy from '@vitejs/plugin-legacy'
 //import { globalExternals } from '@fal-works/esbuild-plugin-global-externals'
+import { dependencies } from './package.json';
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: process.env.ELECTRON=="true" ? './' : "/",
   plugins: [
-    VueDevTools(),
     vue(),
-    Inspect(),
-    Inspector(),
     inject({
         jQuery: 'jquery',
     }),
-    checker({
+    //i18nResources({
+      //path: resolve(__dirname, "src/locales"),
+    //}),
+//    checker({
 //	vueTsc: true,
 //      typescript: true,
-    }),
+//    }), 
     AutoImport({
       imports: ['vue', '@vueuse/core']  ,
       resolvers: [
@@ -41,17 +52,48 @@ export default defineConfig({
       vueTemplate: true,
       cache: true,
     }),
+    Inspect(),
+    Inspector(),
+    VueDevTools(),
+
+    chunkSplitPlugin({
+      strategy: 'default', // 'single-vendor','all-in-one', 'unbundle', 'default'
+      customChunk: (args)=>{
+        // files into pages directory is export in single files
+        let { file, id, moduleId, root } = args;
+		//file = file.replace(/^[\.\/]*/, '');
+		//console.log('root:'+root+' file:'+file);
+	//if (file.startsWith('src/views/billing/affiliates/Tos.vue')) {
+		//return 'views-billing-affilites-tos';
+	//} else 
+	if (file.startsWith('src/views/billing/affiliates/')) {
+		return 'views-billing-affilites';
+        } else if(file.startsWith('src/')){
+		return file.match('src/views/.*'+'/') ? 'views-'+file.split('/')[2] : (file.match('/.*'+'/') ? file.split('/')[1] : 'src');
+	//file = file.substring(4);
+          //file = file.replace(/\.[^.$]+$/, '');
+         // return file;
+        } else if (file.match('node_modules/datatables')) {
+		return 'datatables';
+	}
+        return null;
+      }, 
+      customSplitting: {
+        // `react` and `react-dom` will be bundled together in the `react-vendor` chunk (with their dependencies, such as object-assign)
+        'datatables': ['datatables.net', 'datatables.net-dt', 'datatables.net-bs4'],
+        // Any file that includes `utils` in src dir will be bundled in the `utils` chunk
+        //'utils': [/src\/utils/]
+      }
+    }),
+
     splitVendorChunkPlugin(),
+    //legacy({ targets: ['defaults', 'not IE 11'] })
     VitePWA({ 
 	registerType: 'autoUpdate',
-//      devOptions: {
-//        enabled: true
-//      }
+      devOptions: {
+        enabled: true
+      }
     }),
-    //legacy({ targets: ['defaults', 'not IE 11'] }),
-    /* i18nResources({
-      path: resolve(__dirname, "src/locales"),
-    }) */
   ],
   optimizeDeps: {
       disabled: false,
@@ -61,7 +103,56 @@ export default defineConfig({
       }*/
   },
   build: {
-      commonjsOptions: { include: [] }
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+	//manualChunks: (path) => path.split('/').reverse()[path.split('/').reverse().indexOf('node_modules') - 1] // just a hack to get the next path segment of the last node_modules in path Worked like a charm (works better as an function as it seems)
+/*        manualChunks: (path) => {
+	//console.log(path);
+          if (path.includes('datatables.net')) {
+		return 'vendor-datatables';
+	}
+          if (path.includes('jquery')) {
+		return 'vendor-jquery';
+	}
+          if (path.includes('vue')) {
+		return 'vendor-vue';
+	}
+          if (path.includes('node_modules')) {
+		return 'vendor';
+	}
+	return "index";
+*/
+        manualChunks: {
+          datatables: ['datatables.net-bs', 'datatables.net-bs4', 'datatables.net-buttons', 'datatables.net-buttons-bs', 'datatables.net-buttons-bs4', 'datatables.net-dt', 'datatables.net-responsive-bs', 'datatables.net-responsive-bs4', 'datatables.net-searchpanes-bs', 'datatables.net-select', 'datatables.net-select-bs', 'datatables.net-select-bs4', 'datatables.net-vue3', ],
+	jquery: ['jquery', 'jquery-migrate', 'jquery-simple-pass-meter', 'jquery-sortable', 'jquery-ui', 'jquery-ui-dist', ],
+	vite: ['vite', '@vitejs/plugin-legacy', '@vitejs/plugin-vue', 'vite-plugin-checker', 'vite-plugin-chunk-split', 'vite-plugin-i18n-resources', 'vite-plugin-inspect', 'vite-plugin-pwa', 'vite-plugin-vue-devtools', 'vite-plugin-vue-inspector', 'vitest', '@vitest/coverage-c8', '@vitest/ui', ],
+	vue: ['eslint-plugin-vue', '@fortawesome/vue-fontawesome', '@intlify/unplugin-vue-i18n', 'mosha-vue-toastify', '@unhead/vue', 'vue', '@vue/eslint-config-prettier', 'vue-i18n', 'vue-query', 'vue-recaptcha', 'vue-router', '@vue/test-utils', 'vue-tsc', '@vueuse/core', ],
+	bootstrap: ['admin-lte', 'bootstrap', 'select2', 'select2-bootstrap-theme', 'sweetalert2', '@sweetalert2/theme-bootstrap-4', 'tempusdominus-bootstrap-4', ],
+//          ...renderChunks(dependencies),
+        },
+      },
+    },
+      commonjsOptions: { 
+	include: [] 
+      }
+  },
+  output: { 
+        manualChunks: (path) => {
+          if (path.includes('datatables.net')) {
+		return 'vendor-datatables';
+	}
+          if (path.includes('jquery')) {
+		return 'vendor-jquery';
+	}
+          if (path.includes('vue')) {
+		return 'vendor-vue';
+	}
+          if (path.includes('node_modules')) {
+		return 'vendor';
+	}
+	return "index";
+	}
   },
   resolve: {
     alias: {
