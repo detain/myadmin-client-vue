09a918232fbd46bb404823e576bfc94aab750afc
Joe Huss <detain@interserver.net>
2023-06-28 04:35:03 -0400

    lib updates and added loading the new api site info data in the site store

--- src/stores/site.store.js
+++ src/stores/site.store.js
@@ -9,11 +9,27 @@ export const useSiteStore = defineStore({
         breadcrums: {},
         page_heading: '',
         sidemenu: '',
-        title: ''
+        title: '',
+        modules: {},
+        services: {},
+        serviceTypes: {},
+        serviceCategories: {}
     }),
     actions: {
         getBaseUrl() {
             return baseUrl;
+        },
+        loadInfo() {
+            fetchWrapper
+                .get(baseUrl+'/info')
+                .then((response) => {
+                    console.log("add cc success");
+                    console.log(response);
+                })
+                .error((response) => {
+                    console.log("add cc failed");
+                    console.log(response);
+                });
         },
         setBreadcrums(value) {
             this.breadcrums = value;
