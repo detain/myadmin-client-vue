7da8793b836238aaa252bd711565fe859185f565
Joe Huss <detain@interserver.net>
2023-06-29 14:39:59 -0400

    updates to affilaite routes and affiliate views adding an index list

--- src/router/affiliate.routes.js
+++ src/router/affiliate.routes.js
@@ -1,19 +1,7 @@
-import Affiliate from '@/views/billing/affiliates/Affiliate.vue';
-import Faq from '@/views/billing/affiliates/Faq.vue';
-import DockSetup from '@/views/billing/affiliates/DockSetup.vue';
-import PaymentSetup from '@/views/billing/affiliates/PaymentSetup.vue';
-import RichReport from '@/views/billing/affiliates/RichReport.vue';
-import SalesGraph from '@/views/billing/affiliates/SalesGraph.vue';
-import Signups from '@/views/billing/affiliates/Signups.vue';
-import Status from '@/views/billing/affiliates/Status.vue';
-import Tos from '@/views/billing/affiliates/Tos.vue';
-import TrafficGraph from '@/views/billing/affiliates/TrafficGraph.vue';
-import WebTraffic from '@/views/billing/affiliates/WebTraffic.vue';
-import Banners from '@/views/billing/affiliates/Banners.vue';
-import ViewBanner from '@/views/billing/affiliates/ViewBanner.vue';
+import { Affiliate, Banners, DockSetup, Faq, Layout, PaymentSetup, RichReport, SalesGraph, Signups, Status, Tos, TrafficGraph, ViewBanner, WebTraffic } from '@/views/billing/affiliates';
 
 export default {
-    path: '/affiliate',
+    path: '/affiliates',
     //component: Layout,
     children: [
         { path: '', component: Affiliate },
