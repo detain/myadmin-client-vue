d623a01357a7947b66f6117c14559130b333d3a3
Joe Huss <detain@interserver.net>
2023-08-29 02:12:05 -0400

    update going down to 1060 vue-tsc errors

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Alert } from './Alert.vue';
 export { default as Nav } from './Nav.vue';
 export { default as MainMenu } from './MainMenu.vue';
-export { default as CrudTable } from './CrudTable.vue';
