86663d8d806e498158e5edd70e5c9306ec5733e2
Joe Huss <detain@interserver.net>
2023-09-17 18:49:42 -0400

    updated vite backup

--- vite.config.backup.js
+++ vite.config.backup.js
@@ -1,11 +1,5 @@
-import { rmSync } from 'node:fs'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import { notBundle } from 'vite-plugin-electron/plugin'
-import pkg from './package.json'
-/*
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 //import AutoImport from "unplugin-auto-import/vite";
 //import i18nResources from "vite-plugin-i18n-resources"
@@ -19,7 +13,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import Inspect from 'vite-plugin-inspect';
 //import Inspector from 'vite-plugin-vue-inspector';
 //import TurboConsole from "vite-plugin-turbo-console";
-*/
+
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
     plugins: [
@@ -54,7 +48,7 @@ export default defineConfig({
         }),
         splitVendorChunkPlugin(),
         legacy({ targets: ["defaults", "not IE 11"] }),
-        */
+*/
         VitePWA({
             registerType: 'autoUpdate',
             devOptions: {
@@ -110,83 +104,3 @@ export default defineConfig({
         },
     },
 });
-=======
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  rmSync('dist-electron', { recursive: true, force: true })
-
-  const isServe = command === 'serve'
-  const isBuild = command === 'build'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
-
-  return {
-    plugins: [
-      vue(),
-      electron([
-        {
-          // Main process entry file of the Electron App.
-          entry: 'electron/main/index.ts',
-          onstart({ startup }) {
-            if (process.env.VSCODE_DEBUG) {
-              console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
-            } else {
-              startup()
-            }
-          },
-          vite: {
-            build: {
-              sourcemap,
-              minify: isBuild,
-              outDir: 'dist-electron/main',
-              rollupOptions: {
-                // Some third-party Node.js libraries may not be built correctly by Vite, especially `C/C++` addons, 
-                // we can use `external` to exclude them to ensure they work correctly.
-                // Others need to put them in `dependencies` to ensure they are collected into `app.asar` after the app is built.
-                // Of course, this is not absolute, just this way is relatively simple. :)
-                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
-              },
-            },
-            plugins: [
-              // This is just an option to improve build performance, it's non-deterministic!
-              // e.g. `import log from 'electron-log'` -> `const log = require('electron-log')` 
-              isServe && notBundle(),
-            ],
-          },
-        },
-        {
-          entry: 'electron/preload/index.ts',
-          onstart({ reload }) {
-            // Notify the Renderer process to reload the page when the Preload scripts build is complete, 
-            // instead of restarting the entire Electron App.
-            reload()
-          },
-          vite: {
-            build: {
-              sourcemap: sourcemap ? 'inline' : undefined, // #332
-              minify: isBuild,
-              outDir: 'dist-electron/preload',
-              rollupOptions: {
-                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
-              },
-            },
-            plugins: [
-              isServe && notBundle(),
-            ],
-          },
-        }
-      ]),
-      // Use Node.js API in the Renderer process
-      renderer(),
-    ],
-    server: process.env.VSCODE_DEBUG && (() => {
-      const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-      return {
-        host: url.hostname,
-        port: +url.port,
-      }
-    })(),
-    clearScreen: false,
-  }
-})
->>>>>>> electron-vite-vue/main
