2afc2d0dd3b3ce0ba5a092ca8b5d0b4b9db37652
Joe Huss <detain@interserver.net>
2025-12-31 19:57:00 -0500

    added capacitor

---
+++ android/app/src/main/java/net/interserver/my/MainActivity.java
@@ -0,0 +1,5 @@
+package net.interserver.my;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.interserver.my',
+  appName: 'my-interserver-client',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
