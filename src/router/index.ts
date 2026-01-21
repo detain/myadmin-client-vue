447a4b6777c8614d394f289e4cc855863d590f9b
Joe Huss <detain@interserver.net>
2026-01-21 16:27:15 -0500

    updates working on order license

--- src/router/index.ts
+++ src/router/index.ts
@@ -97,7 +97,7 @@ export const router = createRouter({
                 { path: '', component: () => import('../views/licenses/LicensesList.vue') },
                 //{ path: 'order/:catTag?', component: () => import('../views/licenses/OrderLicense.vue') },
                 { path: 'order', component: () => import('../views/licenses/OrderLicense.vue') },
-                { path: 'order/:catTag', component: () => import('../views/licenses/OrderLicense.vue') },
+                { path: 'order/:catTag(directadmin|softaculous|parallels|cpanel|litespeed|cloudlinux)', component: () => import('../views/licenses/OrderLicense.vue') },
                 { path: ':id(\\d+)', component: () => import('../views/licenses/ViewLicense.vue') },
                 { path: ':id(\\d+)/:link(welcome_email|cancel|invoices|change_ip|change_os)', component: () => import('../views/licenses/ViewLicense.vue') },
             ],
