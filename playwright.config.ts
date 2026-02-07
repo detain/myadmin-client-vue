6ec69f2b7bce4f4d5d069e36d513fbf380870b7b
Joe Huss <detain@interserver.net>
2026-02-07 09:48:17 -0500

    playwright config foix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env'), });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -38,7 +38,7 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
-/*
+        /*
         {
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
