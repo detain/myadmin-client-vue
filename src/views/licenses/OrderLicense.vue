24c081bbdca9dc7d58de88dea182e108e4729683
Joe Huss <detain@interserver.net>
2026-02-09 06:10:30 -0500

    some more work on order license

--- src/views/licenses/OrderLicense.vue
+++ src/views/licenses/OrderLicense.vue
@@ -54,6 +54,12 @@ const getServiceTypes = computed(() => {
             types[serviceId] = serviceTypes.value[serviceId];
             if (catTag.value == 'litespeed') {
                 types[serviceId].services_name = types[serviceId].services_name.replace(/LiteSpeed /i, '');
+            } else if (catTag.value == 'parallels') {
+                types[serviceId].services_name = types[serviceId].services_name.replace(/Plesk v12 /i, '');
+            } else if (catTag.value == 'cpanel') {
+                types[serviceId].services_name = serviceId === '10682' ? `${types[serviceId].services_name} Server` : `${types[serviceId].services_name.replace(/Cloud /i, '')} VPS`;
+            } else if (catTag.value == 'softaculous') {
+                types[serviceId].services_name = serviceId === '5006' ? `${types[serviceId].services_name} Server (External)` : `${types[serviceId].services_name} (External)`;
             }
         }
     }
@@ -549,7 +555,7 @@ watch(
                             <div class="pb-1 pt-3">
                                 <h4 class="text-center"><u>Agree to the offer terms</u></h4>
                                 <p class="text-center text-sm">The subscription will automatically renew after <b>every month at</b> <span class="package_cost text-bold"></span> until canceled.</p>
-                                <p class="text-muted text-xs">By checking this box, you acknowledge that you are purchasing a subscription product that automatically renews <br /><b>( As Per The Terms Outlined Above )</b> and is billed to the credit card you provide today. If you wish to cancel your auto-renewal, you may access the customer portal <a href="https://my.interserver.net" target="__blank" class="link">(Here)</a> select the active service and click the <b>Cancel</b> link or email at: <a href="mailto:billing@interserver.net" class="link">billing@interserver.net</a> or use another method outlined in the <b>Terms and Conditions.</b> By checking the box and clicking Place My Order below, You also acknowledge you have read, understand, and agree to our <a class="link" href="https://www.interserver.net/terms-of-service.html" target="__blank">Terms and Conditions</a> and <a class="link" href="https://www.interserver.net/privacy-policy.html" target="__blank">Privacy Policy</a>.</p>
+                                <p class="text-muted text-xs">By checking this box, you acknowledge that you are purchasing a subscription product that automatically renews <b>( As Per The Terms Outlined Above )</b> and is billed to the credit card you provide today. If you wish to cancel your auto-renewal, you may access the customer portal <a href="https://my.interserver.net" target="__blank" class="link">(Here)</a> select the active service and click the <b>Cancel</b> link or email at: <a href="mailto:billing@interserver.net" class="link">billing@interserver.net</a> or use another method outlined in the <b>Terms and Conditions.</b> By checking the box and clicking Place My Order below, You also acknowledge you have read, understand, and agree to our <a class="link" href="https://www.interserver.net/terms-of-service.html" target="__blank">Terms and Conditions</a> and <a class="link" href="https://www.interserver.net/privacy-policy.html" target="__blank">Privacy Policy</a>.</p>
                                 <p class="icheck-success text-bold text-center">
                                     <input id="tos" v-model="tos" type="checkbox" name="tos" style="margin: 0 5px; display: inline" value="yes" />
                                     <label for="tos" class="d-inline text-center">I have read the terms above and I agree.</label>
@@ -557,6 +563,7 @@ watch(
                             </div>
                             <div class="row">
                                 <div class="controls col-md-12 text-center">
+                                    <router-link :to="`/licenses/order/${catTag}`" class="btn btn-custom btn-sm px-3 py-2 mr-2" data-toggle="tooltip" title="Go Back" style=""><i class="fa fa-arrow-left">&nbsp;</i>&nbsp;Go Back&nbsp;&nbsp;</router-link>
                                     <input type="submit" name="Submit" value="Place Order" class="btn btn-sm btn-green px-3 py-2" />
                                 </div>
                             </div>
