ced1d882647923551964d78a43a0cfc569c48d27
Joe Huss <detain@interserver.net>
2026-03-03 23:42:18 -0500

    update moving dirs around to better match upstream layout

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -6,13 +6,37 @@ import { fileURLToPath, URL } from 'node:url';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    main: {},
-    preload: {},
+    main: {
+        build: {
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, 'electron/main/index.ts'),
+                },
+            },
+        },
+    },
+    preload: {
+        build: {
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, 'electron/preload/index.ts'),
+                },
+            },
+        },
+    },
     renderer: {
+        root: '.',
+        build: {
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, 'index.html'),
+                },
+            },
+        },
         resolve: {
             alias: {
-                '@renderer': resolve('src/renderer/src'),
-                '@': fileURLToPath(new URL('./src/renderer/src', import.meta.url)),
+                '@renderer': resolve('src'),
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
         plugins: [
