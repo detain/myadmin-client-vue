d6fb9040193ad4e7a13ff6be0efc9e53d691e695
Joe Huss <detain@interserver.net>
2026-03-28 22:38:54 -0400

    updates fixing some localization issues

--- vite.config.ts
+++ vite.config.ts
@@ -99,7 +99,7 @@ export default defineConfig({
         alias: {
             //'@': path.resolve(__dirname, './src'),
             '@': fileURLToPath(new URL('./src', import.meta.url)),
-            'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
+            'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
         },
     },
     server: {
